edc18961fc55dc2121114c5f456e8ea10cf23ccc
rossenger <113732492+rossenger@users.noreply.github.com>
2023-02-19 21:32:18 -0600

    Added JS UI interaction

---
+++ js/index.js
@@ -0,0 +1,27 @@
+console.log("Your index.js file is loaded correctly!");
+
+$(document).ready(function() {
+    $('.fadeIn1').css('display', 'none');
+    $('.fadeIn1').fadeIn(3000);
+})
+
+$(document).ready(function() {
+    $('.fadeIn2').css('display', 'none');
+    $('.fadeIn2').delay('500').fadeIn(2000);
+})
+
+$(document).ready(function() {
+    $('.fadeIn3').css('display', 'none');
+    $('.fadeIn3').delay('1000').fadeIn(1000);
+})
+
+$(document).ready(function() {
+    $('.fadeIn4').css('display', 'none');
+    $('.fadeIn4').delay('1500').fadeIn(2000);
+})
+
+$(document).ready(function() {
+    $('.fadeIn5').css('display', 'none');
+    $('.fadeIn5').delay('2000').fadeIn(2000);
+})
+
